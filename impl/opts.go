bd0af45e350edae97b7cb4113ef5d72c105c9f5e
Badari Burli <bburli@suki.ai>
2025-08-26 13:00:12 -0700

    throwing an error if option values are invalid

--- impl/opts.go
+++ impl/opts.go
@@ -1,29 +1,33 @@
 package impl
 
 import (
+	"fmt"
 	"time"
-
-	"github.com/handcoding-labs/redis-stream-client-go/configs"
 )
 
-type RecoverableRedisOption func(*RecoverableRedisStreamClient)
+type RecoverableRedisOption func(*RecoverableRedisStreamClient) error
 
 // WithLBSIdleTime sets the time after which a message is considered idle and will be recovered
 func WithLBSIdleTime(idleTime time.Duration) RecoverableRedisOption {
-	return func(r *RecoverableRedisStreamClient) {
+	return func(r *RecoverableRedisStreamClient) error {
 		// idleTime must be greater than 2 * heartbeat interval at least
-		if idleTime != 0 && idleTime > (2*r.hbInterval) {
-			r.lbsIdleTime = idleTime
-		} else {
-			// if the value is not valid, set it to the default
-			r.lbsIdleTime = configs.DefaultLBSIdleTime
+		if idleTime == 0 || idleTime <= (2*r.hbInterval) {
+			return fmt.Errorf("idleTime must be greater than 2 * heartbeat interval at least")
 		}
+
+		r.lbsIdleTime = idleTime
+		return nil
 	}
 }
 
 // WithLBSRecoveryCount sets the number of messages to fetch at a time during recovery
 func WithLBSRecoveryCount(count int) RecoverableRedisOption {
-	return func(r *RecoverableRedisStreamClient) {
+	return func(r *RecoverableRedisStreamClient) error {
+		if count <= 0 {
+			return fmt.Errorf("recovery count must be greater than 0")
+		}
+
 		r.lbsRecoveryCount = count
+		return nil
 	}
 }
