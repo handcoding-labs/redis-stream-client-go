dab7a6353d0998300e749022f6236480afc94b57
Badari Burli <burli.badari@gmail.com>
2026-02-28 12:54:22 +0530

    making capture_handler thread safe

--- test/capture_handler.go
+++ test/capture_handler.go
@@ -3,11 +3,13 @@ package test
 import (
 	"context"
 	"log/slog"
+	"sync"
 )
 
 // captureHandler is a custom slog.Handler that captures log messages in a slice for testing purposes.
 type captureHandler struct {
 	output *[]string
+	mu     sync.Mutex
 }
 
 func (h *captureHandler) Enabled(ctx context.Context, level slog.Level) bool {
@@ -15,7 +17,9 @@ func (h *captureHandler) Enabled(ctx context.Context, level slog.Level) bool {
 }
 
 func (h *captureHandler) Handle(ctx context.Context, record slog.Record) error {
+	h.mu.Lock()
 	*h.output = append(*h.output, record.Message)
+	h.mu.Unlock()
 	return nil
 }
 
